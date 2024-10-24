2bcacaea7c3379185ddb9760bdab52190c9bcbd1
Dragoonoia <joaoaugustoreul@hotmail.com>
2024-10-24 17:16:52 -0300

    2d movement + jump + gamepad

--- Assets/player/scripts/PlayerMovement.cs
+++ Assets/player/scripts/PlayerMovement.cs
@@ -1,30 +1,136 @@
-using UnityEngine;
 using Fusion;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed = 7f;
+    [Header("Jumponthecadillac")]
+    [SerializeField] private float jumpForce = 12f;
+    [SerializeField] private float jumpTime = 0.5f;
+
+    [SerializeField] public bool isFacingRight;
+    [SerializeField] private GameObject sprite;
+
+    [Header("ithrewitontheGROUND")]
+    [SerializeField] private float extraheight = 0.25f;
+    [SerializeField] private LayerMask whatIsGround;
+
     private Rigidbody2D body;
+    private Collider2D collider;
+    private float moveInput;
+
+
+    private bool isJumpin;
+    private bool isFallin;
+    private float jumptimeCounter;
+
+    private RaycastHit2D groundhit;
+
+    
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-    }
-    void Start()
-    {
-        
+        collider = GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocityY);
+        Move();
+        Jump();
+    }
 
-        if (Input.GetKey(KeyCode.W))
+    #region Movement Functions
+    private void Move()
+    {
+        moveInput = UserInput.instance.moveInput.x;
+
+        body.linearVelocity = new Vector2(moveInput * moveSpeed, body.linearVelocity.y);
+        if (moveInput > 0 || moveInput < 0)
         {
-            body.linearVelocityY = speed;
+            TurnCheck();
+        }
 
-           
+    }
+
+    #endregion
+    #region Jump Functions
+    private void Jump()
+    {
+       if (UserInput.instance.controls.Jumpin.Jumpin.WasPressedThisFrame() && isGrounded())
+        {
+            //pushed :D
+            isJumpin = true;
+            jumptimeCounter = jumpTime;
+            body.linearVelocity = new Vector2(body.linearVelocityX, jumpForce);
+        }
+
+       if (UserInput.instance.controls.Jumpin.Jumpin.IsPressed())
+        {
+            if (jumptimeCounter >0 && isJumpin)
+            {
+                body.linearVelocity = new Vector2(body.linearVelocityX, jumpForce);
+                jumptimeCounter -= Time.deltaTime;
+            }
+
+            else
+            {
+                isJumpin = false;
+            }
+            
+        }
+       if (UserInput.instance.controls.Jumpin.Jumpin.WasReleasedThisFrame())
+        {
+            isJumpin = false;
         }
     }
+
+    #endregion
+    #region turn functions
+    private void TurnCheck()
+    {
+        if (UserInput.instance.moveInput.x > 0 && !isFacingRight)
+        {
+            Turn();
+        }
+        else if (UserInput.instance.moveInput.x <0 && isFacingRight)
+        {
+            Turn();
+        }
+    }
+    private void Turn()
+    {
+        if (isFacingRight)
+        {
+            Vector3 rotator = new Vector3(transform.position.x, 180f, transform.position.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = false;
+        }
+        else
+        {
+            Vector3 rotator = new Vector3(transform.position.x, 0f, transform.position.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingRight = true;
+        }
+    }
+    #endregion
+    #region Ground Check
+    private bool isGrounded()
+    {
+        groundhit = Physics2D.BoxCast(collider.bounds.center, collider.bounds.size, 0f, Vector2.down, extraheight, whatIsGround);
+
+        if (groundhit.collider != null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    #endregion
+
 }
