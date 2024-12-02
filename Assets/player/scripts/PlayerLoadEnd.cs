bd7c44f077058dcab978dfcd6ad7675d759f2397
Dragoonoia <joaoaugustoreul@hotmail.com>
2024-12-02 17:16:41 -0300

    update3

--- Assets/player/scripts/PlayerLoadEnd.cs
+++ Assets/player/scripts/PlayerLoadEnd.cs
@@ -10,6 +10,7 @@ public class PlayerLoadEnd : NetworkBehaviour
     [SerializeField] private GameObject Loose;
     private bool Won;
     Finisher LineFinish;
+    bool bonkers;
     void Awake()
     {
         
@@ -17,9 +18,6 @@ public class PlayerLoadEnd : NetworkBehaviour
             Win = GameObject.FindGameObjectWithTag("WinScreen");
             Loose = GameObject.FindGameObjectWithTag("LooseScreen");
             LineFinish = GameObject.FindGameObjectWithTag("FinishLine").GetComponent<Finisher>();
-            Loading.gameObject.SetActive(false);
-            Win.gameObject.SetActive(false);
-            Loose.gameObject.SetActive(false);
             Won = false;
         
     }
@@ -27,7 +25,16 @@ public class PlayerLoadEnd : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Won && LineFinish.P1Finished == true && HasStateAuthority)
+        if (Runner.SessionInfo.PlayerCount == 2 && !bonkers)
+        {
+            Loading.gameObject.SetActive(false);
+            Win.gameObject.SetActive(false);
+            Loose.gameObject.SetActive(false);
+            bonkers = true;
+        }
+
+
+        if (!Won && LineFinish.P1Finished && HasStateAuthority)
         {
             Loose.gameObject.SetActive(true);
         }
@@ -35,7 +42,7 @@ public class PlayerLoadEnd : NetworkBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == ("FinishLine") && HasStateAuthority)
+        if (collision.gameObject.tag == ("FinishLine") && HasStateAuthority && !LineFinish.P1Finished)
         {
             Win.gameObject.SetActive(true);
             LineFinish.P1Finished = true;
